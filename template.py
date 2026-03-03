44ec102470d43baf6e2cbd49da947663fc109b51
meghan <meghan.lakkaraju@gmail.com>
2026-03-03 08:10:16 +0530

    initial commit

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+import logging
+
+# Logging configuration to track creation process
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "TradingBot"
+
+list_of_files = [
+    ".github/workflows/.gitkeep", # For future CI/CD pipelines
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/dsp_filter.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/backtester.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/training_pipeline.py",
+    f"src/{project_name}/pipeline/prediction_pipeline.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
